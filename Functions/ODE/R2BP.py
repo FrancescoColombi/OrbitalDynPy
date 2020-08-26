4c819b2dd07fd140c4cae3930f4e2027ff87fff6
FranceColo_acer <colombi.francesco93@gmail.com>
2020-08-26 12:20:51 +0200

    Reorganize directory structure

---
+++ Functions/ODE/R2BP.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+# Restricted 2-Body Problem
+def R2BP_ode(X, t, mu):
+    x = X[0]
+    y = X[1]
+    z = X[2]
+    vx = X[3]
+    vy = X[4]
+    vz = X[5]
+
+    r = np.linalg.norm(X[0:2])
+
+    X_dot = [
+        vx,
+        vy,
+        vz,
+        -mu / (r ^ 3) * x,
+        -mu / (r ^ 3) * y,
+        -mu / (r ^ 3) * z
+    ]
+    return X_dot
+
+# Restricted 2-Body Problem plus perturbations
+
+# Rotational Dynamics driven by gravity gradient and considering rigid body
+
+# Relative motion
+
