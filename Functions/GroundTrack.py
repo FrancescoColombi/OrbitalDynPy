4b91d11ced06631000920ab4624423d56438c500
FranceColo_acer <colombi.francesco93@gmail.com>
2020-09-05 19:58:53 +0200

    add radius as output of GroundTrack function

--- Functions/GroundTrack.py
+++ Functions/GroundTrack.py
@@ -1,29 +1,11 @@
-import sys
-# sys.path.append('/OrbitalDynPy/Utilities')
-
-import numpy as np
-import pandas as pd
-from scipy.integrate import odeint
-import matplotlib as mpl
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
-# import matplotlib.image as mpimg
-# from PIL import Image
-
-from spiceypy import spiceypy as spice
-
-from Functions.ODE.R2BP import R2BP_dyn
-from Functions.Utilities.KeplerianParameters import kp2rv
-from Functions.Utilities.TimeConversion import jd2GMST
-
-'''
+"""
 # This function returns the ground track over a planet surface given input time-position coordinates
 #
 # INPUTS            Unit        Description
 # --------------    ---------   ------------------------------------------------------------
 # tt                [sec]       Time past from reference time t_0 for PMST_0 estimation
 #                               Array size = [n]
-# rr                [km]        Position vectors in Equatorial Frame
+# rr                [length]    Position vectors in Equatorial Frame
 #                               Array size = [3, n]
 # t_0               [sec]       Reference time
 # PMST_0            [h]         Prime Meridion Sidereal Time is the time angle of the Prime
@@ -36,16 +18,20 @@ from Functions.Utilities.TimeConversion import jd2GMST
 # delta             [deg]       Declination in Equatorial Frame. Array size = [n]
 # latitude          [deg]       Latitude. Array size = [n]
 # longitude         [deg]       Longitude. Array size = [n]
-'''
+# radius            [length]    Radius. Array size = [n]
+"""
+
+
+import numpy as np
 
 
 def GroundTrack(tt, rr, t_0, PMST_0, omega_planet, rr_axis=1):
-    # rr shall be used in the following as an array with shape [3, n]
+    # this function requires rr to be an array with shape [3, n]
     try:
         if np.shape(rr)[0] != 3 and np.shape(rr)[1] != 3:
-            raise Exception('Position vector array shall have shape as [3, n] or [n, 3]')
+            raise Exception('Position array shall have shape equal to [3, n] or [n, 3]')
         elif np.shape(rr)[0] != 3 and np.shape(rr)[1] == 3:  # if input rr has shape [n, 3] --> transpose it
-            rr = rr.T
+            rr = np.transpose(rr)
     except Exception as err:
         print(err)
 
@@ -54,12 +40,14 @@ def GroundTrack(tt, rr, t_0, PMST_0, omega_planet, rr_axis=1):
     delta = np.zeros(np.size(tt))
     latitude = np.zeros(np.size(tt))
     longitude = np.zeros(np.size(tt))
+    radius = np.zeros(np.size(tt))
 
     # Initial Prime Meridian angle of the planet [rad]
     theta_0 = PMST_0 * np.pi / 12
 
     for n in range(np.size(tt)):
         r = np.linalg.norm(rr[:, n])
+        radius[n] = r
 
         # Compute declination [rad]
         delta[n] = np.arcsin(rr[2, n] / r)
@@ -93,83 +81,6 @@ def GroundTrack(tt, rr, t_0, PMST_0, omega_planet, rr_axis=1):
     delta = delta * 180 / np.pi
     latitude = latitude * 180 / np.pi
     longitude = longitude * 180 / np.pi
-    return alpha, delta, latitude, longitude
+    return alpha, delta, latitude, longitude, radius
 
 
-def test_GroundTrack():
-    # Constants
-    R_earth = 0.63781600000000E+04
-    mu_earth = 0.59736990612667E+25 * 6.67259e-20
-
-    # Sidereal time of Earth Rotation [sec]
-    ST_earth_rot = (23 + (56 + 4.09 / 60) / 60) / 24 * 86400
-    # Earth rotation rate [rad/sec]
-    omega_earth = 2 * np.pi / ST_earth_rot
-
-    # Orbit parameters
-    altitude = 400.
-    eccentricity = 0.0
-    incl = 98.0
-    Omega = 10.0
-    omega = 30.0
-    theta = 0.0
-    kp0 = [R_earth + altitude, eccentricity, incl, Omega, omega, theta]
-    rr0, vv0 = kp2rv(kp0, mu_earth)
-    print('Keplerian parameter:         ', kp0)
-    print('Initial position [km]:       ', rr0)
-    print('Initial velocity [km/sec]:   ', vv0)
-
-    # Reference time
-    kernel_dir = 'D:/Francesco/Spice_kernels/'
-    meta_kernel = kernel_dir + 'meta_kernel.tm'
-    spice.furnsh(meta_kernel)
-    date0 = "2020 jan 01 12:00:00"
-    jd0 = spice.utc2et(date0) / 86400
-    spice.kclear()
-
-    # Reference Prime Meridian Sidereal Time
-    GMST_0 = jd2GMST(jd0)
-
-    # Orbit propagation
-    X0 = np.hstack((rr0, vv0))
-    tspan = [0, 5 * 3600]
-    t_out = np.linspace(tspan[0], tspan[1], 5000)
-    y_out = odeint(R2BP_dyn, X0, t_out, args=(mu_earth,), rtol=1e-10, atol=1e-10)
-
-    rr_orb = y_out[:, 0:3].T
-    vv_orb = y_out[:, 3:6].T
-
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
-    ax.plot(rr_orb[0, :], rr_orb[1, :], rr_orb[2, :])
-    ax.scatter(0, 0, 0, color='blue')
-    ax.scatter(rr_orb[0, 0], rr_orb[1, 0], rr_orb[2, 0], color='red')
-    ax.set_xlabel('x [km]')
-    ax.set_ylabel('y [km]')
-    ax.set_zlabel('z [km]')
-    ax.set_title('Orbital propagation in Equatorial Frame')
-    plt.show()
-
-    # Ground Track
-    t_0 = jd0 * 86400  # [sec]
-    tt = t_0 + t_out
-    alpha, delta, lat, long = GroundTrack(tt, rr_orb, t_0, GMST_0, omega_earth)
-
-    # print(alpha[range(5)])
-    # print(delta[range(5)])
-    print(lat[range(10)])
-    print(long[range(10)])
-
-    fig2 = plt.figure()
-    ax = fig2.subplots(1, 1)
-    ax.scatter(long, lat, c=tt)
-    ax.scatter(long[0], lat[0], color='red', label='Initial condition')
-    ax.set_xlim([-180, 180])
-    ax.set_ylim([-90, 90])
-    plt.grid()
-    plt.legend()
-    plt.show()
-    return
-
-
-# test_GroundTrack()
--- Scripts/R2BP/R2BP_test.py
+++ Scripts/R2BP/R2BP_test.py
@@ -1,7 +1,112 @@
-from Functions.ODE.R2BP import *
-from Functions.Utilities.KeplerianParameters import *
-from Functions.GroundTrack import *
+import numpy as np
+import pandas as pd
+from scipy.integrate import odeint
 
-# test_orbit()
+import matplotlib as mpl
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+# import matplotlib.image as mpimg
+# from PIL import Image
+
+from spiceypy import spiceypy as spice
+
+
+from Functions.GroundTrack import GroundTrack
+from Functions.ODE.R2BP import R2BP_dyn
+from Functions.Utilities.KeplerianParameters import kp2rv
+from Functions.Utilities.TimeConversion import jd2GMST
+
+def test_GroundTrack():
+    # Constants
+    R_earth = 0.63781600000000E+04
+    mu_earth = 0.59736990612667E+25 * 6.67259e-20
+
+    # Sidereal time of Earth Rotation [sec]
+    ST_earth_rot = (23 + (56 + 4.09 / 60) / 60) / 24 * 86400
+    # Earth rotation rate [rad/sec]
+    omega_earth = 2 * np.pi / ST_earth_rot
+
+    # Orbit parameters
+    altitude = 550.
+    a = R_earth + altitude
+    # a = 26600
+    eccentricity = 0.
+    incl = 98.0
+    Omega = 0.0
+    omega = 0.0
+    theta = 0.0
+    kp0 = [a, eccentricity, incl, Omega, omega, theta]
+    rr0, vv0 = kp2rv(kp0, mu_earth)
+    print('Keplerian parameter:         {0}'.format(kp0))
+    print('Initial position [km]:       [{:.3f}, {:.3f}, {:.3f}]'.format(rr0[0], rr0[1], rr0[2]))
+    print('Initial velocity [km/sec]:   [{:.3f}, {:.3f}, {:.3f}]'.format(vv0[0], vv0[1], vv0[2]))
+    T_orb = 2 * np.pi * np.sqrt(a**3 / mu_earth)
+    print(T_orb)
+
+    # Reference time
+    kernel_dir = 'D:/Francesco/Spice_kernels/'
+    meta_kernel = kernel_dir + 'meta_kernel.tm'
+    spice.furnsh(meta_kernel)
+    date0 = "2020 jan 01 12:00:00"
+    jd0 = spice.utc2et(date0) / 86400
+    spice.kclear()
+
+    # Reference Prime Meridian Sidereal Time
+    GMST_0 = jd2GMST(jd0)
+
+    # Orbit propagation
+    X0 = np.hstack((rr0, vv0))
+    t0 = 0.
+    tf = 2 * T_orb
+    t_out = np.linspace(t0, tf, 5000)
+    y_out = odeint(R2BP_dyn, X0, t_out, args=(mu_earth,), rtol=1e-10, atol=1e-10)
+
+    rr_orb = y_out[:, 0:3].T
+    vv_orb = y_out[:, 3:6].T
+
+    deltat = 1*3600
+    t_marker = np.arange(t0, tf, deltat)
+    y_marker = odeint(R2BP_dyn, X0, t_marker, args=(mu_earth,), rtol=1e-10, atol=1e-10)
+    rr_marker = y_marker[:, 0:3].T
+
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.plot(rr_orb[0, :], rr_orb[1, :], rr_orb[2, :])
+    ax.scatter(0, 0, 0, color='blue')
+    ax.scatter(rr_orb[0, 0], rr_orb[1, 0], rr_orb[2, 0], color='red')
+    ax.set_xlabel('x [km]')
+    ax.set_ylabel('y [km]')
+    ax.set_zlabel('z [km]')
+    ax.set_title('Orbital propagation in Equatorial Frame')
+    plt.show()
+
+    # Ground Track
+    t_0 = jd0 * 86400  # [sec]
+    tt = t_0 + t_out
+    alpha, delta, lat, long, _ = GroundTrack(tt, rr_orb, t_0, GMST_0, omega_earth)
+
+    tt_mark = t_0 + t_marker
+    alpha, delta, lat_mark, long_mark, _ = GroundTrack(tt_mark, rr_marker, t_0, GMST_0, omega_earth)
+
+    # print(alpha[range(5)])
+    # print(delta[range(5)])
+    # print(lat[range(10)])
+    # print(long[range(10)])
+
+    fig2 = plt.figure()
+    ax = fig2.subplots(1, 1)
+    ax.scatter(long, lat, c=tt)
+    ax.scatter(long_mark, lat_mark, color='blue')
+    ax.scatter(long[0], lat[0], color='red', label='Initial condition')
+    ax.set_xlim([-180, 180])
+    ax.set_ylim([-90, 90])
+    plt.grid()
+    plt.legend()
+    plt.show()
+    return
+
+
+# test_GroundTrack()
+
 
 test_GroundTrack()
